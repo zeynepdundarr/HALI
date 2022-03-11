6f1935b30b8ee6ee6a7679021b02a94089b447a4
zeynepdundarr <idundar17@ku.edu.tr>
2022-03-12 00:22:21 +0300

    backend is set up

---
+++ halisaha/index.js
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+AppRegistry.registerComponent(appName, () => App);
